0689e92d0baf96c2ae79a4ac8c11e04c6dc8c6a0
leejhy <120650622+leejhy@users.noreply.github.com>
2025-05-11 18:43:02 +0900

    [MOB] FIX: fix crash in header_section by modifying styles

--- lib/styles/text_styles.dart
+++ lib/styles/text_styles.dart
@@ -15,13 +15,13 @@ class AppTextStyles{
   );
   static const dDayStyle = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 30,
+    fontSize: 24,
     fontWeight: FontWeight.bold,
     letterSpacing: 2.5,
   );
   static const dateStyle = TextStyle(
     fontFamily: 'Inter',
-    fontSize: 17,
+    fontSize: 16,
     fontWeight: FontWeight.normal,
     letterSpacing: -0.1,
     color: Color(0xff6B7280),
