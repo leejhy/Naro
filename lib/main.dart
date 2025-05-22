5157b1d51ae595bed66245eb88b13eb71721048f
leejhy <120650622+leejhy@users.noreply.github.com>
2025-05-22 21:22:30 +0900

    [MOB] ADD: add easy_localization #43

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,30 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await DatabaseHelper.database;
   await MobileAds.instance.initialize();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseAuth.instance.signInAnonymously();
-  runApp(ProviderScope(child: const MyApp()));
+  final deviceLocale = WidgetsBinding.instance.platformDispatcher.locale;
+  final initialLocale = deviceLocale.languageCode == 'ko'
+      ? const Locale('ko')
+      : const Locale('en');
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ko')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      startLocale: initialLocale,
+      child: ProviderScope(child: const MyApp())
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,6 +38,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       routerConfig: router,
       title: 'Naro - 미래의 나에게 쓰는 편지',
