79cfe6469ad2bf206b7a87f761d74c3ee9b7e6fe
leejhy <120650622+leejhy@users.noreply.github.com>
2025-05-24 20:40:41 +0900

    [MOB] MOD: modify dateformat on header_section in HomeScreen

--- lib/widgets/home/header_section.dart
+++ lib/widgets/home/header_section.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:naro/styles/text_styles.dart';
 import 'dart:math';
 import 'package:easy_localization/easy_localization.dart';
-
-const List<String> noLetterMessages = [
-  '오고 있는 편지가 없어요.\n지금 이 순간을 담아보는 건 어때요?',
-  '아직 도착 예정인 편지가 없어요.\n오늘의 마음을 기록해 보는 건 어때요?',
-  '아직 편지를 보내지 않았어요.\n미래의 내가 기다리고 있을지 몰라요.',
-  '편지함이 고요해요.\n오늘의 감정을 남겨보는 건 어때요?',
-  '지금의 내가, 미래의 나에게\n가장 큰 힘이 되어줄 수 있어요.',
-  '언젠가 불안한 날의 내가\n오늘의 당신을 기억하게 될 거예요.',
-  '언젠가 힘들어할 나에게\n따뜻한 한마디를 남겨보는 건 어때요?',
-];
+import 'package:naro/utils/utils.dart';
 
 class HeaderSection extends StatefulWidget {
   const HeaderSection({
@@ -42,8 +33,8 @@ class _HeaderSectionState extends State<HeaderSection> {
   @override
   Widget build(BuildContext context) {
     final dDay = widget.dDay;
-    final arrivalDate = widget.arrivalDate;
-
+    final arrivalDate = getLocalizedDateString(widget.arrivalDate, context);
+    // print(getLocalizedDateString(widget.arrivalDate, context));
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       sliver: SliverToBoxAdapter(
@@ -65,15 +56,14 @@ class _HeaderSectionState extends State<HeaderSection> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
+            
             children: [
               if (dDay > 0) ... [
                 Text('header.upcoming'.tr(), style: AppTextStyles.headingStyle),
                 Text('header.d_day'.tr(namedArgs: {'day': dDay.toString()}), style: AppTextStyles.dDayStyle),
                 SizedBox(height: 4),
                 Text('header.arrival_date'.tr(namedArgs: {
-                  'year': arrivalDate.year.toString(),
-                  'month': arrivalDate.month.toString(),
-                  'day': arrivalDate.day.toString(),
+                  'date': arrivalDate,
                 }), style: AppTextStyles.dateStyle),
               ] else if (dDay == 0)... [
                 Center(
